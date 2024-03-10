9961bd436aaf0b06618a54f2c09e427dbb5a32d3
tomo1227 <tomo.soccer1227@gmail.com>
2024-03-10 07:01:58 +0000

    feat✨ wsサーバのhandlerを構築

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,48 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+
+	"github.com/tomo1227/chat_app_backend/internal/adapter/ws/handler"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		// オリジンの確認を行わない場合は、常に true を返す
-		return true
-	},
-}
-
-func handleWebSocketConnection(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer conn.Close()
-
-	fmt.Println("Client Connected")
-
-	for {
-		// メッセージの読み込み
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// 受信したメッセージを出力
-		fmt.Printf("Received Message: %s\n", p)
-
-		// 受信したメッセージをそのまま返す
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func main() {
-	http.HandleFunc("/ws", handleWebSocketConnection)
-
-	fmt.Println("WebSocket server is running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprint(w, "hello")
+	// })
+	http.HandleFunc("/ws", handler.NewWebsocketHandler().Handle)
+	port := "8080"
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil); err != nil {
+		log.Fatalf("Serve Error: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/tomo1227/chat_app_backend
 
 go 1.21.5
+
+require github.com/gorilla/websocket v1.5.1
+
+require golang.org/x/net v0.21.0 // indirect
